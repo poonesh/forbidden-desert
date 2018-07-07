9ecc3d10e154518a674b1ca4a0840d72060d9b38
pooneh <poonehshooshtari@gmail.com>
2018-07-06 20:06:27 -0400

    Completed and debugged print() and move() methods

--- _site/board.js
+++ _site/board.js
@@ -12,8 +12,8 @@ class Board {
 		this.height = height;
 		this.initialSandIndices = [[2, 0], [1, 1], [3, 1], [0, 2], [4, 2], [1, 3], [3, 3], [2, 4]];
 		this.emptyIndex = [Math.floor(this.width/2), Math.floor(this.height/2)];
-		this.boardArr = []
-
+		this.boardArr = [];
+		this.initializeBoard();
 	}
 
 	initializeBoard(){
@@ -40,8 +40,9 @@ class Board {
 				let x = this.emptyIndex[0];
 				let y = this.emptyIndex[1];
 				
-				if (y + 1 < this.height){
+				if ( + 1 < this.height){
 					this.boardArr[x][y] = this.boardArr[x][y+1];
+					this.boardArr[x][y].sandCovered += 1;
 					this.boardArr[x][y+1] = null;
 					this.emptyIndex = [x, y+1];
 				}
@@ -53,19 +54,21 @@ class Board {
 
 				if (y - 1 >= 0){
 					this.boardArr[x][y] = this.boardArr[x][y-1];
-					this.bardArr[x][y-1] = null;
+					this.boardArr[x][y].sandCovered += 1;
+					this.boardArr[x][y-1] = null;
 					this.emptyIndex = [x, y-1];
 				}
 			}
 		} else if (direction === "LEFT"){
-			for (let k = 0; k<count; k++){
+			for (let k=0; k<count; k++){
 				let x = this.emptyIndex[0];
 				let y = this.emptyIndex[1];
 
-				if (x + 1 < self.width){
+				if (x + 1 < this.width){
 					this.boardArr[x][y] = this.boardArr[x+1][y];
+					this.boardArr[x][y].sandCovered += 1;
 					this.boardArr[x+1][y] = null;
-					this.emptyIndex = [this.emptyIndex[0]+1, this.emptyIndex[1]];
+					this.emptyIndex = [x+1, y];
 				}
 			}
 		} else if (direction === "RIGHT"){
@@ -75,8 +78,9 @@ class Board {
 
 				if (x - 1 >= 0){
 					this.boardArr[x][y] = this.boardArr[x-1][y];
+					this.boardArr[x][y].sandCovered += 1;
 					this.boardArr[x-1][y] = null;
-					this.emptyIndex = [this.emptyIndex[0]-1, this.emptyIndex[1]];
+					this.emptyIndex = [x-1, y];
 				}
 			}
 
@@ -85,15 +89,14 @@ class Board {
 
 	print(){
 		let str = "";
-		for(let i=0; i<this.width; i++){
+		for(let j=0; j<this.height; j++){
 			let tempStr = " ";
-			for(let j=0; j<this.height; j++){
+			for(let i=0; i<this.width; i++){
 				if(this.boardArr[i][j] === null){
 					tempStr += "X ";
-				}else if(this.boardArr[i][j].sandCovered === 0){
-					tempStr += "0 ";
-				}else if(this.boardArr[i][j].sandCovered !== 0){
-					tempStr += "1 ";
+				}else{
+					let numSand = this.boardArr[i][j].sandCovered
+					tempStr += numSand + " ";
 				}
 			}
 			tempStr += "\n";
@@ -104,8 +107,5 @@ class Board {
 }
 
 
-let board = new Board(5, 5);
-board.initializeBoard();
-console.log(board.print());
 
 
